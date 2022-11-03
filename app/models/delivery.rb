8abcb0c2f97c76635e2ba5e7ccd5a73ea04de2c7
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2022-11-03 21:20:28 +0000

    Model logic

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id                    :integer          not null, primary key
+#  arrived               :boolean
+#  description           :string
+#  details               :text
+#  supposed_to_arrive_on :date
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  user_id               :integer
+#
 class Delivery < ApplicationRecord
+  validates(:description, { :presence => true })
+  
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
 end
