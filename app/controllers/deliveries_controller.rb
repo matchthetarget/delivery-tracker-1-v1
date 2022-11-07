6deaec1b2c6d17e17b4dc3379873f2777c96ef44
Jelani Woods <jelani@firstdraft.com>
2022-11-07 10:01:07 -0600

    Add homepage specs

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -27,6 +27,7 @@ class DeliveriesController < ApplicationController
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
     the_delivery.description = params.fetch("query_description")
     the_delivery.details = params.fetch("query_details")
+    the_delivery.arrived = false
 
     if the_delivery.valid?
       the_delivery.save
